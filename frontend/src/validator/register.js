49fb6900e0892833b29f59c97f6c30ff2e6e0722
Rinshad <rinshad@teamioss.in>
2025-12-16 12:18:53 +0530

    feat: add validation utilities for email, username, and password fields

---
+++ frontend/src/validator/register.js
@@ -0,0 +1,128 @@
+export const loginUsernameValidator = (item) => {
+    const rules = {
+        required: {
+            value: true,
+            message: 'this_field_is_required'
+        },
+    };
+
+    if (item.type === 'text') {
+        rules.minLength = {
+            value: item?.validation?.min,
+            message: `${('min_length_is')} ${item?.validation?.min}`,
+        };
+        rules.maxLength = {
+            value: item?.validation?.max,
+            message: `${('max_length_is')} ${item?.validation?.max} `,
+        };
+        rules.pattern = {
+            value: /^[A-Za-z0-9]+$/,
+            message: "invalid_format",
+        }
+    } else if (item.type === 'password') {
+        rules.minLength = {
+            value: item.validation.minLength,
+            message: `${('min_length_is')} ${item.validation.minLength}`,
+        };
+    }
+    return rules;
+}
+
+export const loginPasswordValidator = (item, t) => {
+    const rules = {
+        required: {
+            value: true,
+            message: 'this_field_is_required'
+        },
+    };
+
+    if (item.validation.minLength) {
+        rules.minLength = {
+            value: item.validation.minLength,
+            message: `${('min_length_is')} ${item.validation.minLength}`
+        };
+    }
+
+    const regexParts = [];
+    if (item.validation.specialChar) {
+        regexParts.push("(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"|,.<>?])"); // At least one special character
+    }
+    if (item.validation.number) {
+        regexParts.push("(?=.*[0-9])"); // At least one number
+    }
+    if (item.validation.mixedCase) {
+        regexParts.push("(?=.*[A-Z])"); // At least one capital letter
+    }
+
+    if (regexParts.length > 0) {
+        rules.pattern = {
+            value: new RegExp(`^${regexParts.join("")}.*$`),
+            message: 'Almost there! Check the password requirements again.',
+        };
+    }
+    return rules;
+};
+
+export const passwordRules = (item) => {
+    const rules = []
+    if (item.minLength) {
+        rules.push('minLength')
+    }
+    if (item.spChar) {
+        rules.push('specialChar');
+    }
+    if (item.number) {
+        rules.push('number');
+    }
+    if (item.mixedCase) {
+        rules.push('capital');
+        rules.push('lowercase');
+    }
+    return rules
+}
+
+export const validateAge = (value) => {
+    const currentDate = new Date();
+    const selectedDate = new Date(value);
+    const minAge = 18; // Change this to your desired minimum age
+    if (currentDate.getFullYear() - selectedDate.getFullYear() < minAge) {
+        return ("you_must_be_at_least_18_years_old.");
+    }
+
+    return true;
+};
+
+export const forgetPasswordValidator = (item, t) => {
+    const rules = {
+        required: {
+            value: true,
+            message: t('this_field_is_required')
+        },
+    };
+    if (item.passwordPolicy.minLength) {
+        rules.minLength = {
+            value: item.passwordPolicy.minLength,
+            message: `${t('min_length_is')} ${item.passwordPolicy.minLength}`
+        }
+    }
+    if (item.passwordPolicy.spChar) {
+        rules.pattern = {
+            value: /[!@#$%^&*()_+\-=\[\]{};':"|,.<>?]/,
+            message: t('must_contain_at_least_one_special_character')
+        };
+    }
+    if (item.passwordPolicy.number) {
+        rules.patternNumber = {
+            value: /[0-9]/,
+            message: t('must_contain_at_least_one_number')
+        };
+    }
+    if (item.passwordPolicy.mixedCase) {
+        rules.patternCapital = {
+            value: /[A-Z]/,
+            message: t('must_contain_at_least_one_capital_letter')
+        };
+    }
+
+    return rules;
+}
