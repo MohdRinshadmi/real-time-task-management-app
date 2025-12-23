e58301f884d9704f795ddbf6a8a6c3d9b5495af2
Rinshad <rinshad@teamioss.in>
2025-12-23 11:05:20 +0530

    feat: refactor MainDashboard to use Redux for authentication state and simplify header rendering

--- frontend/src/views/dashboard/MainDashboard.jsx
+++ frontend/src/views/dashboard/MainDashboard.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import Dashboard from '../../components/dashboard/Dashboard';
 import Header from '../../components/header/Header';
 import TaskBoard from '../../components/taskBoard/TaskBoard';
+import { useSelector } from 'react-redux';
 
 const MainDashboard = () => {
-  const login = true;
-  return (
-    <div className="min-h-screen relative" style={{ background: 'linear-gradient(180deg, #E6ECFF 0%, #F4F7FF 40%, #FFFFFF 100%)' }}>
-      {/* Header overlays above TaskBoard */}
-      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', zIndex: 10 }}>
-        <Header />
-      </div>
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
-      {/* TaskBoard background visually wraps header */}
-      <main className="pt-12">
-        {login ? (
-          <TaskBoard />
-        ) : (
-          <div>
-            <Dashboard />
-          </div>
-        )}
+  const showHeader = !isLoggedIn;
+
+  return (
+    <div>
+      {showHeader && (
+        <header>
+          <Header />
+        </header>
+      )}
+
+      <main>
+        {isLoggedIn ? <TaskBoard /> : <Dashboard />}
       </main>
     </div>
   );
 };
+
+
 
 export default MainDashboard;
