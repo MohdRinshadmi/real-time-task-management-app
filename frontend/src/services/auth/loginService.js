9f747756b5e16aa69056120fde70f793248ea7d6
Rinshad <rinshad@teamioss.in>
2025-12-27 12:35:31 +0530

    login service response structure changes and api response structure change

--- frontend/src/services/auth/loginService.js
+++ frontend/src/services/auth/loginService.js
@@ -2,24 +2,17 @@ import api from '../../api/api';
 import { setAccessToken } from '../../global/localStorage';
 import { setLoggedIn } from '../../store/store';
 
-/**
- * Login user with email and password
- * @param {string} email
- * @param {string} password
- * @param {function} dispatch - Redux dispatch function
- * @returns {Promise}
- */
 export const loginService = async (email, password, dispatch) => {
   try {
     const { data } = await api.post('/login', { email, password });
     console.log('datattat', data);
     
-    if (data.data.token) {
-      setAccessToken(data.data.token);
+    if (data.token) {
+      setAccessToken(data.token);
     }
 
-    if (dispatch && typeof data.data.isLoggedIn === 'boolean') {
-      dispatch(setLoggedIn(data.data.isLoggedIn));
+    if (dispatch && typeof data.isLoggedIn === 'boolean') {
+      dispatch(setLoggedIn(data.isLoggedIn));
     }
 
     return data;
