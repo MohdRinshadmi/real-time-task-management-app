0783773eec08d0de09059c6840757d9b26b4fe55
Rinshad <rinshad@teamioss.in>
2025-12-29 17:21:29 +0530

    feat: set up theme provider and integrate react-query for improved state management

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,12 +7,24 @@ import { Flip } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from 'react-redux';
 import store from './store/store.js';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} >
-    <App />
-    <ToastContainer position="bottom-center" pauseOnHover={false} autoClose={2000} theme='colored' transition={Flip} />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ToastContainer position="bottom-center" pauseOnHover={false} autoClose={2000} theme='colored' transition={Flip} />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 )
