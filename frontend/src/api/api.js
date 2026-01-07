371d0f1063dbbbcfd1bc06d7d38dcff711c4abdb
Rinshad <rinshad@teamioss.in>
2026-01-07 12:02:58 +0530

    fix: Correct import statement for BASE_URL in API configuration

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { toast } from "react-toastify";
-import BASE_URL from "../config/config";
+import { BASE_URL } from "../config/config";
 
 const instance = axios.create({
   baseURL: BASE_URL,
