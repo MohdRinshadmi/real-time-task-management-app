5a7f76f89b9f480f4f6609dd1744215ff1e39288
Rinshad <rinshad@teamioss.in>
2026-01-09 17:53:24 +0530

    feat: Add GoogleId field to user schema for enhanced authentication

--- backend/src/nosql/userModel.js
+++ backend/src/nosql/userModel.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phoneNumber: { type: String },
+  GoogleId: { type: String, unique: true, sparse: true },
   privacyAccepted: { type: String, default: "false" },
   password: { type: String},
 }, {
